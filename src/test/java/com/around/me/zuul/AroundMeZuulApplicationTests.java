50aaa83fa3de53e8ed358d98e49f6e42e697f110
seo <seo@seoui-MacBookPro.local>
2020-11-18 21:32:47 +0900

    Initial commit

---
+++ src/test/java/com/around/me/zuul/AroundMeZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.around.me.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundMeZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
